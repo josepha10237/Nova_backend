7d13b5607c792dbd853bc2ec9c88b8a6808d464e
Marie <josephabomba10@gmail.com>
2026-09-06 09:59:21 +0100

    Actualiser setting.py

--- Nova1/settings.py
+++ Nova1/settings.py
@@ -152,4 +152,4 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CSRF_TRUSTED_ORIGINS = [
-    'https://novabend-production.up.railway.app',]
+    'https://web-production-1527f.up.railway.app/',]
