0659589be40272f2df8afd08f30fd5320b4429f8
Marie <josephabomba10@gmail.com>
2026-09-08 22:32:07 +0100

    DEPLOI3  views.py

--- Nova1/urls.py
+++ Nova1/urls.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+# Petite fonction pour afficher un message à la racine de l'API
+def api_home(request):
+    return HttpResponse("✅ Le backend Django de Nova fonctionne parfaitement ! Les services API sont disponibles sous /api/.")
 
 urlpatterns = [
+    path('', api_home), # 👈 Gère la page d'accueil de Railway
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
 
-# Force Django à servir les fichiers médias même en production (essentiel pour tes PDF)
+# Routage des fichiers médias
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
