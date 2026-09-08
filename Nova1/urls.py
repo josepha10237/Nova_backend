eb3f9c2202d4a0ab6cbd37583e831f68b8be6e7a
Marie <josephabomba10@gmail.com>
2026-09-08 06:08:44 +0100

    Actualiser  admin.py

--- Nova1/urls.py
+++ Nova1/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # <- Point d'entrée pour Angular (http://127.0.0.1:8000/api/epreuves/)
+    path('api/', include('api.urls')),  # Remplace 'api' par le nom exact de ton app si c'est 'nova'
 ]
-
-# Permet de servir les fichiers PDF uploadés en mode développement
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
