3285fa4a3f0afdfecbbeb7b27105b47abed5205b
Marie <josephabomba10@gmail.com>
2026-09-09 06:29:50 +0100

    DEPLOIE4  views.py

--- Nova1/settings.py
+++ Nova1/settings.py
@@ -153,4 +153,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CSRF_TRUSTED_ORIGINS = [
     'https://*.up.railway.app',
+    'https://nova-pour-tous1.netlify.app',
 ]
